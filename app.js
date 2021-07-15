bbd05bcb6a5d742b196c40e6aa5e6151b815b425
AbithaRao <70374395+AbithaRao@users.noreply.github.com>
2021-07-15 10:52:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+window.onscroll = function () {
+    var info4Y = document.getElementById('info4').offsetTop;
+    var info3Y = document.getElementById('info3').offsetTop;
+    var info2Y = document.getElementById('info2').offsetTop;
+    var info1Y = document.getElementById('info1').offsetTop;
+    var currentScrollPos = window.pageYOffset;
+    if(currentScrollPos + 80 < info3Y){
+        document.getElementById('ed4').style.opacity = "1";
+        document.getElementById('ed3').style.opacity = "0.7";
+        document.getElementById('ed2').style.opacity = "0.7";
+        document.getElementById('ed1').style.opacity = "0.7";
+    }    
+    else if(currentScrollPos + 80 >= info3Y && currentScrollPos + 80 < info2Y){
+        document.getElementById('ed4').style.opacity = "0.7";
+        document.getElementById('ed3').style.opacity = "1";
+        document.getElementById('ed2').style.opacity = "0.7";
+        document.getElementById('ed1').style.opacity = "0.7";
+    }
+    else if(currentScrollPos +80 >= info2Y && currentScrollPos + 80 < info1Y){
+        document.getElementById('ed4').style.opacity = "0.7";
+        document.getElementById('ed3').style.opacity = "0.7";
+        document.getElementById('ed2').style.opacity = "1";
+        document.getElementById('ed1').style.opacity = "0.7";
+    }   
+    else{
+        document.getElementById('ed4').style.opacity = "0.7";
+        document.getElementById('ed3').style.opacity = "0.7";
+        document.getElementById('ed2').style.opacity = "0.7";
+        document.getElementById('ed1').style.opacity = "1";
+    }
+}
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
